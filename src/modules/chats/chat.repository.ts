785995171e9b38d72f10959198af0b3212d26777
Shoxrux Rashidov <mrrashidov@hotmail.com>
2023-06-30 08:54:29 +0500

    fix(chats): added knex query interface

--- src/modules/chats/chat.repository.ts
+++ src/modules/chats/chat.repository.ts
@@ -5,39 +5,40 @@ import BaseRepositoryInterface, {
   Update,
 } from '~/global/base-repository.interface';
 import { CursorInterface } from '~/global/pagination.interface';
+import { Knex as KnexInterface } from 'knex';
 
 export class ChatRepository implements BaseRepositoryInterface<Chat> {
   constructor(@InjectKnex() private readonly knex: Knex) {}
 
-  private get chats() {
+  private get query(): KnexInterface.QueryBuilder {
     return this.knex<Chat>('chats');
   }
 
   async create(dto: Create<Chat>): Promise<Chat> {
-    const result = await this.chats.insert(dto).returning('*');
+    const result = await this.query.insert(dto).returning('*');
 
     return result[0];
   }
 
   findById(id: number): Promise<Chat> {
-    return this.chats.where({ id }).first();
+    return this.query.where({ id }).first();
   }
 
   find(filter?: CursorInterface): Promise<Chat[]> {
-    return this.chats.select('*');
+    return this.query.select('*');
   }
 
   first(where: Chat): Promise<Chat> {
-    return this.chats.where(where).first();
+    return this.query.where(where).first();
   }
 
   async update(id: number, dto: Update<Chat>): Promise<Chat> {
-    const result = await this.chats.where({ id }).update(dto).returning('*');
+    const result = await this.query.where({ id }).update(dto).returning('*');
 
     return result[0];
   }
 
   delete(id: number): Promise<Chat> {
-    return this.chats.where({ id }).del();
+    return this.query.where({ id }).del();
   }
 }
